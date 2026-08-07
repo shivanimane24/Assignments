e8f2e82c6bb678508caa61cba4786568cad7e6a8
Shivani <shivanimane2411@gmail.com>
2026-08-07 14:41:41 +0530

    Assignments

---
+++ day01-my-profile.js
@@ -0,0 +1,12 @@
+
+let name= "Shivani Mane";
+let role= "Manual Tester";
+let experience= 4;
+let tool= "Core Banking System";
+let learningGoal= "To automate repetitive account checks";
+console.log("Name:", name);
+console.log("Role:", role);
+console.log("Experience:", experience);
+console.log("Tool:", tool);
+console.log("Learning Goal:", learningGoal);
+
