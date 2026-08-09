4658eaadc75335399bfbf7cad4e334cf0cb89229
Shivani <shivanimane2411@gmail.com>
2026-08-09 16:37:32 +0530

    Add type checking demo

---
+++ type-checking-demo.js
@@ -0,0 +1,19 @@
+let firstName = "Shivani";              // String
+let lastName = "Mane";                // String
+let userId = 1001;                   // Number
+let age = 28;                        // Number
+let email = "shivani@test.com";     // String
+let isActive = true;                 // Boolean
+let loginCount = 15;                 // Number
+let averageScore = 87.5;             // Number
+let hasAccess = true;                // Boolean
+
+console.log("Type of:" ,typeof firstName);
+console.log("Type of:" ,typeof lastName);
+console.log("Type of:" ,typeof userId);
+console.log("Type of:" ,typeof age);
+console.log("Type of:" ,typeof email);
+console.log("Type of:" ,typeof isActive);
+console.log("Type of:" ,typeof loginCount);
+console.log("Type of:" ,typeof averageScore);
+console.log("Type of:" ,typeof hasAccess);
