9b70c4f90096637d799abc4780debac15c2d80d5
Shivani <shivanimane2411@gmail.com>
2026-08-09 16:36:21 +0530

    Add user profile datatypes

---
+++ user-profile-datatypes.js
@@ -0,0 +1,19 @@
+let firstName = "Shivani";              // String
+let lastName = "Mane";                // String
+let userId = 1001;                   // Number
+let age = 28;                        // Number
+let email = "shivani@test.com";     // String
+let isActive = true;                 // Boolean
+let loginCount = 15;                 // Number
+let averageScore = 87.5;             // Number
+let hasAccess = true;                // Boolean
+
+console.log("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("User Id:", userId);
+console.log("Age:", age);
+console.log("Email:", email);
+console.log("Active User:", isActive);
+console.log("Login Count:", loginCount);
+console.log("Average Score:", averageScore);
+console.log("Has Access:", hasAccess);
